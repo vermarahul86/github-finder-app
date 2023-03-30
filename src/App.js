68571757a30012f0d0144649b004fff689971e26
vermarahul86 <i.vermarahul@gmail.com>
2023-03-30 21:21:03 +0530

    54. Adding reducers.

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ import { GithubProvider } from './context/github/GithubContext';
 function App() {
   return (
     <GithubProvider>
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar/>
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>  
-            <Route path='/about' element={<About/>}/>  
-            <Route path='/notfound' element={<NotFound/>}/>  
-            <Route path='/*' element={<NotFound/>}/>  
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar/>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>  
+              <Route path='/about' element={<About/>}/>  
+              <Route path='/notfound' element={<NotFound/>}/>  
+              <Route path='/*' element={<NotFound/>}/>  
+            </Routes>
+          </main>
+          <Footer/>
+        </div>
+      </Router>
     </GithubProvider>
   );
 }
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,5 +1,6 @@
-import { createContext } from "react";
-import { useEffect, useState } from 'react';
+import { createContext, useReducer, useState, useEffect } from "react";
+import githubReducer from "./GithubReducer";
+
 
 const GithubContext = createContext()
 
@@ -7,8 +8,12 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({children}) => {
-    const[users, setUsers] = useState([])
-    const[loading, setLoading] = useState(true)
+    const initialState = {
+        users: [],
+        loading: true,
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async() => {
         const response = await fetch(`${GITHUB_URL}/users`,{
@@ -18,11 +23,13 @@ export const GithubProvider = ({children}) => {
     })
     const data = await response.json()
    
-    setUsers(data)
-    setLoading(false)
+    dispatch({
+        type: 'GET_USERS',
+        payload: data,
+    })
     
     }
-    return <GithubContext.Provider value = {{users, loading, fetchUsers}}>
+    return <GithubContext.Provider value = {{users: state.users, loading: state.loading, fetchUsers}}>
         {children}
     </GithubContext.Provider>
 }
